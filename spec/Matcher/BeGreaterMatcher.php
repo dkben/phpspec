da3fc6340f03a93ecda8e18202e1c97a5f785869
WENI HSU <hsu.weni@gmail.com>
2019-09-14 12:08:33 +0800

    6-Coding up the Custom Matcher

--- spec/Matcher/BeGreaterMatcher.php
+++ spec/Matcher/BeGreaterMatcher.php
@@ -2,7 +2,40 @@
 
 namespace spec\Matcher;
 
-class BeGreaterMatcher
+use PhpSpec\Exception\Example\FailureException;
+use PhpSpec\Matcher\BasicMatcher;
+
+final class BeGreaterMatcher extends BasicMatcher
 {
+    protected function matches($subject, array $arguments): bool
+    {
+        return $subject > $arguments[0];
+    }
+
+    protected function getFailureException(string $name, $subject, array $arguments): FailureException
+    {
+        return new FailureException(sprintf(
+           'Expected %d to be greater than %d',
+           $subject,
+           $arguments[0]
+        ));
+    }
+
+    protected function getNegativeFailureException(string $name, $subject, array $arguments): FailureException
+    {
+        return new FailureException(sprintf(
+            'Expected %d to not be greater than %d',
+            $subject,
+            $arguments[0]
+        ));
+    }
+
+    public function supports(string $name, $subject, array $arguments): bool
+    {
+        return in_array($name, ['beGreater', 'beGreaterThan'])
+            && is_numeric($subject)
+            && count($arguments) === 1
+            && is_numeric($arguments[0]);
+    }
 
 }
