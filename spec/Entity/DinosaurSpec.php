b9f0d1dbb19f61df78a74fee3b593b2703c8cca1
WENI HSU <hsu.weni@gmail.com>
2019-09-14 10:17:17 +0800

    4-Custom Inline Matcher

--- spec/Entity/DinosaurSpec.php
+++ spec/Entity/DinosaurSpec.php
@@ -3,11 +3,28 @@
 namespace spec\App\Entity;
 
 use App\Entity\Dinosaur;
+use PhpSpec\Exception\Example\FailureException;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
 class DinosaurSpec extends ObjectBehavior
 {
+    public function getMatchers(): array
+    {
+        return [
+            'returnZero' => function($subject) {
+                if ($subject !== 0) {
+                    throw new FailureException(sprintf(
+                        'Returned value should be zero, got "%s"',
+                        $subject
+                    ));
+                }
+
+                return true;
+            }
+        ];
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Dinosaur::class);
@@ -16,6 +33,11 @@ class DinosaurSpec extends ObjectBehavior
     function it_should_default_to_zero_length()
     {
         $this->getLength()->shouldReturn(0);
+    }
+
+    function it_should_default_to_zero_length_using_custom_matcher()
+    {
+        $this->getLength()->shouldReturnZero();
     }
 
     function it_should_allow_to_set_length()
